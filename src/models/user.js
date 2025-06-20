2459a4c8ae2a9a84c5695a0888f3d088212aa4c5
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-06-19 18:53:05 -0700

    Adding Auth - 2

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   email:      { type: String, required: true, unique: true },
@@ -21,4 +21,4 @@ userSchema.methods.verifyPassword = function(pw) {
   return bcrypt.compareSync(pw, this.passwordHash);
 };
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
