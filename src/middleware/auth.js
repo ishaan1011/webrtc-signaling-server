da8263eb2d789e07a62f63c0c375f9212e1aa4cb
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-06-19 18:55:27 -0700

    Adding Auth - 3

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,6 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
-module.exports = function(req, res, next) {
+export default function authMiddleware(req, res, next) {
   const auth = req.headers.authorization;
   if (!auth || !auth.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'No token provided' });
